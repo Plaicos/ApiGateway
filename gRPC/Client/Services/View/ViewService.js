7707ce8858ddecb37b441aa5dfd14d657c0290c3
RodrigoHBD <54192949+RodrigoHBD@users.noreply.github.com>
2020-05-18 18:05:58 -0300

    WIP

--- gRPC/Client/Services/View/ViewService.js
+++ gRPC/Client/Services/View/ViewService.js
@@ -3,7 +3,7 @@ var Service = require("../Service")
 class ViewService extends Service {
     Package = "ViewPackage";
     Name = "View";
-    Uri = "0.0.0.0:5000";
+    Uri = "52.67.162.158:5000";
     Credential = require("grpc").credentials.createInsecure();
 }
 
